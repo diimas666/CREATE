9229ea767334705141f0382ecfa9ba965da69aa2
dima tikhtey <uu36548@gmail.com>
2024-03-28 22:45:42 +0200

    new commit

--- js/header.js
+++ js/header.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    document.querySelector(".h1-header").classList.toggle("h1-header_active", true);
+    document.querySelector(".btn-head").classList.toggle("btn-head_active", true);
+    document.querySelector(".logo").classList.toggle("logo_active", true);   
 
 
 
 
-
-
-
-    
 });

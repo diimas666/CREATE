6b817ea358eb6f55b631835d2969ffbe15cd7574
dima tikhtey <uu36548@gmail.com>
2024-03-30 15:42:35 +0200

    seo-secto

--- js/header.js
+++ js/header.js
@@ -5,6 +5,43 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelector(".logo").classList.toggle("logo_active", true);   
 
 
+    // hanburger 
+    const hamburger = document.querySelector('.hamburger');
+    const links = document.querySelector('.menu');
 
+        function clickHamburger() {
+            hamburger.addEventListener("click", function() {
+                links.classList.toggle('menu_active');
+            });
 
+            document.addEventListener("click", function(event) {
+                const isClickInsideHamburger = hamburger.contains(event.target);
+                const isClickInsideLinks = links.contains(event.target);
+
+                if (!isClickInsideHamburger && !isClickInsideLinks) {
+                    links.classList.remove('menu_active');
+                }
+            });
+        }
+
+clickHamburger();
+
+ // hanburger КОНЕЦ 
+//  наблюдатель за секциями 
+const allSections = document.querySelectorAll(".section");
+function revealSection(entries, observe){
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.remove(" section--hidden");
+        observe.unobserve(entries[0].target);
+    }
+    
+}
+const sectionObserver = new IntersectionObserver(revealSection, {threshold:0.15});
+
+allSections.forEach(function(section){
+    sectionObserver.observe(section);
+    section.classList.add("section--hiden")
+});
+
+// конец  наблюдатель за секциями 
 });
